140c24b5124aaa3d38f46d066ed4beb80d4e35b7
Abhinav <60320192+blacksmithop@users.noreply.github.com>
2023-12-28 07:16:17 +0000

    feat: Gradio -> Streamlit

--- app.py
+++ app.py
@@ -1,42 +1,11 @@
-import gradio as gr
-import os
+import streamlit as st
 from utils.wolvesville import Wolvesville
+from utils.models import Role
+from typing import List
 
-wolf = Wolvesville()
+api = Wolvesville()
 
-
-with gr.Blocks() as roleTab:
-    gr.Markdown("# Roles")
-    roles = wolf.getRoles()
-    with gr.Row():
-        for role in roles[:5]:  # slow loading
-            with gr.Group():
-                gr.Image(
-                    role.image.url,
-                    scale=0.025,
-                )
-                gr.Markdown(
-                    f"**Name**: {role.name}\n\n**Team**: {role.team}\n\n**Aura**: {role.aura}\n\n{role.description}"
-                )
-
-
-with gr.Blocks() as otherTab:
-    gr.Markdown("# Emojis")
-    emojis = wolf.getEmojis()
-    with gr.Row():
-        for emoji in emojis[:5]:  # slow loading
-            with gr.Group():
-                gr.Image(
-                    emoji.urlPreview,
-                    scale=0.025,
-                )
-                gr.Markdown(
-                    f"**Name**: {emoji.name}\n\n**Rarity**: {emoji.rarity}\n\n**Event**: {emoji.event}"
-                )
-
-demo = gr.TabbedInterface([roleTab, otherTab], ["Roles", "Other"])
-
-demo.launch()
-
-if __name__ == "__main__":
-    demo.launch()
+roles: List[Role] = api.getRoleRoleIcons()
+for role in roles[:5]:
+    st.markdown(f"<img src=\"{role.image.url}\" style=\"height: 100px; width:100px;\"/>", unsafe_allow_html=True)
+    
