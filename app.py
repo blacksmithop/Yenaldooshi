71e836066223ac927aa21d55bade1eaf8e9ae0d6
Abhinav <60320192+blacksmithop@users.noreply.github.com>
2023-12-28 07:36:00 +0000

    feat: Move code

--- app.py
+++ app.py
@@ -1,24 +1,7 @@
 import streamlit as st
 from utils.wolvesville import Wolvesville
-from utils.models import Role
-from typing import List
 
 api = Wolvesville()
 
-roles: List[Role] = api.getRoleRoleIcons()
 
-st.title("Roles")
-
-for role in roles[:5]:
-    st.markdown(f"<img src=\"{role.image.url}\" style=\"height: 100px; width:100px;\"/>", unsafe_allow_html=True)
-    
-    st.subheader(role.name)
-    st.text(role.aura)
-    st.text(role.description)
-
-    if role.icons != []:
-        with st.expander("See role icons"):
-            for icon in role.icons:
-                st.markdown(f"<img src=\"{icon}\" style=\"height: 100px; width:100px;\"/>", unsafe_allow_html=True)
-
-    st.divider()
+st.title("Wolvesville App")
