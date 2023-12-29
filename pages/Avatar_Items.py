d6061513126c7b0873bd624b1f9d26e829a62bb3
Abhinav <60320192+blacksmithop@users.noreply.github.com>
2023-12-29 10:06:01 +0000

    feat: Use helper function

--- pages/Avatar_Items.py
+++ pages/Avatar_Items.py
@@ -1,32 +1,38 @@
 import streamlit as st
 from utils.wolvesville import Wolvesville
 from utils.models import AvatarItems, AvatarItemSets
-from utils.helper import getTitle
+from utils.helper import chunks, getTitle, groupItemCategories
 from typing import List
 
 api = Wolvesville()
 
-items: List[AvatarItems] = api.getItems()
-itemSets: List[AvatarItemSets] = api.getItemAsSets()
+_items: List[AvatarItems] = api.getItems()
+itemGroup = groupItemCategories(items=_items)
+
+itemAvatarSets: List[AvatarItemSets] = api.getItemAsSets()
 
 st.markdown(getTitle("Avatar Items"), unsafe_allow_html=True)
 
-for item in items[:5]:
-    st.markdown(
-        f'<img src="{item.imageUrl}" style="height: 100px; width:100px;"/>',
-        unsafe_allow_html=True,
-    )
+CHUNK_SIZE = 4
 
-    itemType = item.type.title()
-    st.subheader(itemType)
-    rarity = item.rarity.title()
-    st.markdown(rarity)
-    st.button(f"{item.costInGold} 🪙", key=item.id)
+for itemType, itemSets in itemGroup.items():
+    itemType = itemType.title()
+    with st.expander(f"See {itemType}"):
+        for items in chunks(itemSets[:8], CHUNK_SIZE):
+            cols = st.columns(CHUNK_SIZE)
+            for item, col in zip(items, cols):
+                col.markdown(
+                    f'<img src="{item.imageUrl}" style="height: 100px; width:100px;"/>',
+                    unsafe_allow_html=True,
+                )
+                rarity = item.rarity.title()
+                col.markdown(rarity)
+                col.button(f"{item.costInGold} 🪙", key=item.id)
 
-    st.divider()
+                col.divider()
 
 st.markdown(getTitle("Item Collections"), unsafe_allow_html=True)
-for item in itemSets[:5]:
+for item in itemAvatarSets:
     st.markdown(
         f'<img src="{item.promoImageUrl}" style="height: 75%; width:75%;"/>',
         unsafe_allow_html=True,
