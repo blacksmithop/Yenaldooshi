46c11a67744ae367cdce30bca3c88a18ddadf270
Abhinav <60320192+blacksmithop@users.noreply.github.com>
2023-12-29 05:44:23 +0000

    feat: Change layout

--- pages/Roles.py
+++ pages/Roles.py
@@ -1,31 +1,39 @@
 import streamlit as st
 from utils.wolvesville import Wolvesville
 from utils.models import Role
+from utils.helper import chunks, getTitle
 from typing import List
 
 api = Wolvesville()
 
 roles: List[Role] = api.getRoleRoleIcons()
 
-st.title("Roles")
+st.markdown(getTitle("Roles"), unsafe_allow_html=True)
+st.divider()
 
-for role in roles[:5]:
-    st.markdown(
-        f'<img src="{role.image.url}" style="height: 100px; width:100px;"/>',
-        unsafe_allow_html=True,
-    )
+CHUNK_SIZE = 4
 
-    st.subheader(role.name)
-    aura = role.aura.title()
-    st.markdown(aura)
-    st.markdown(role.description)
+for roles in chunks(roles, CHUNK_SIZE):
+    cols = st.columns(CHUNK_SIZE)
+    for role, col in zip(roles, cols):
+        col.markdown(
+            f'<img src="{role.image.url}" style="height: 100px; width:100px;"/>',
+            unsafe_allow_html=True,
+        )
 
-    if role.icons != []:
-        with st.expander("See role icons"):
-            for icon in role.icons:
-                st.markdown(
-                    f'<img src="{icon}" style="height: 100px; width:100px;"/>',
-                    unsafe_allow_html=True,
-                )
+        col.subheader(role.name)
+        aura = role.aura.title()
+        col.markdown(aura)
+        col.markdown(role.description)
+
+        if role.icons != []:
+            icons = role.icons
+            with st.expander(f"See {role.name} icons"):
+                cols = st.columns(len(icons))
+                for icon, col in zip(icons, cols):
+                    col.markdown(
+                        f'<img src="{icon}" style="height: 100px; width:100px;"/>',
+                        unsafe_allow_html=True,
+                    )
 
     st.divider()
